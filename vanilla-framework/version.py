efbd848c1c347661571e85e9650f667f38e5b0a8
Pedro Avalos Jimenez <pedro.avalosjimenez@canonical.com>
2026-02-27 12:49:46 -0500

    ci: I give up, let's assume django/flask passes it in

---
+++ vanilla-framework/version.py
@@ -0,0 +1,5 @@
+# Imagine this is a Python file that loads a template
+
+VANILLA_FRAMEWORK_VERSION = "4.43.0"  # renovate: vanilla-framework-latest
+
+# VANILLA_FRAMEWORK_VERSION is passed to context, and . is replaced with _
